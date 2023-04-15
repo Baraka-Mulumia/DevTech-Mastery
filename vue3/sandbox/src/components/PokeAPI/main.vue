8ca4f60faa618d2862db62d3316f539b2f506d80
Baraka Mshindi Mulumia <65751779+baraka-mulumia@users.noreply.github.com>
2023-04-15 10:58:47 +0300

    feat: :sparkles: Add pokedex fetch api call

---
+++ vue3/sandbox/src/components/PokeAPI/main.vue
@@ -0,0 +1,29 @@
+<script>
+const API_URL = 'https://pokeapi.co/api/v2/pokemon?limit=151'
+
+export default {
+  data: () => ({
+    pokedex: [1, 2, 4]
+  }),
+
+  methods: {
+    async fetchPokemon() {
+      this.pokedex = await fetch(API_URL).then((response) => response.json())
+    }
+  },
+
+  created() {
+    this.fetchPokemon()
+  }
+}
+</script>
+
+<template>
+  <h1>Pokemon App</h1>
+
+  <button @click="fetchPokemon">Fetch Pokemon</button>
+
+  <pre>
+    {{ pokedex }}
+  </pre>
+</template>
